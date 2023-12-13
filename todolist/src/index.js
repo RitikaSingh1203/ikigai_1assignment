b94eb4f474624be9b2c7c63571ac7d697d3c328a
RitikaSingh1203 <singh26.juhi@gmail.com>
2023-12-13 22:15:55 +0530

    first commit

---
+++ todolist/src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+var destination = document.querySelector("#container");
+ReactDOM.render(
+  <div>
+    <todolist />
+  </div>,
+  destination
+);
